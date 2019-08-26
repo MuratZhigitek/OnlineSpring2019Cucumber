38e84c7de0b99b1bf9b6c83b6fb49736b34953cb
MuratZhigitek <zhigitek@gmail.com>
2019-08-26 18:19:32 -0400

    TagsUpdated

--- src/test/java/com/vytrack/runners/CukesRunner1.java
+++ src/test/java/com/vytrack/runners/CukesRunner1.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "html:target/default-cucumber-reports",
                 "rerun:target/rerun.txt"
         },
-       tags = {" @login_with_outline_2"},
+       tags = {"@login_with_outline_2"},
         features = {"src/test/resources/features/activities" //to specify where are the features
         },
         //feature contains scenarios
